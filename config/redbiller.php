33b07fb83d6b4e1281975c69a59094cd260c1196
Sanusi Mubaraq <mubaraqsanusi908@gmail.com>
2022-05-26 00:13:03 +0100

    Update and rename config.php to redbiller.php

---
+++ config/redbiller.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * You can place your custom package configuration in here.
+ */
+return [
+      /**
+     * Secret Key From Paystack Dashboard
+     *
+     */
+    'secretKey' => getenv('REDBILLER_SECRET_KEY'),
+];
